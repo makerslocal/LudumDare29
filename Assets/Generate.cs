cddd31094a6c2457890277dfcca83f0ea2231ce6
Wolfenhex <wolfenhex@gmail.com>
2014-04-26 04:03:56 -0500

    Started working on level generation.

---
+++ Assets/Generate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		int count = Random.Range (20, 30);
+
+		for (int i = 0; i < count; i++) {
+
+			int x = Random.Range (-10, 10);
+			int y = Random.Range (-10, 10);
+
+			GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+			quad.transform.position = new Vector2(x, y);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
