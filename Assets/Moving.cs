e2a43a533f3ec77a6f1c7a9718f384bc529e0eee
Wolfenhex <wolfenhex@gmail.com>
2014-04-26 15:05:11 -0500

    Pushing collision matrix and some level generation code.

---
+++ Assets/Moving.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public abstract class Moving : MonoBehaviour {
+
+	protected bool Move(int X, int Y) {
+		
+		X += Mathf.RoundToInt (transform.position.x);
+		Y += Mathf.RoundToInt (transform.position.y);
+		
+		if (X < 0) {
+			return false;
+		}
+		
+		if (Y < 0) {
+			return false;
+		}
+		
+		if (X >= Map.Width) {
+			return false;
+		}
+		
+		if (Y >= Map.Height) {
+			return false;
+		}
+		
+		if (Map.Walls [X, Y] != null) {
+			return false;
+		}
+		
+		transform.position = new Vector2 (X, Y);
+
+		return true;
+	}
+}
