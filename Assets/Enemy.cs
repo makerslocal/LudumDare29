af8cdb1ed50d38991436d31381838d391a8f802b
Wolfenhex <unknown-user@fe80::102e:3a6:67e3:f100%16>
2014-04-26 02:19:23 -0500

    Starting LD29.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {	
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			controller.Move (new Vector2(0, 1));
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			controller.Move (new Vector2(0, -1));
+		}
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			controller.Move (new Vector2(-1, 0));
+		}
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			controller.Move (new Vector2(1, 0));
+		}
+	}
+}
