3150a81ece5ec130718f4574ac54c93010c5feff
David Brooks <brodavi@gmail.com>
2014-04-28 08:31:14 -0500

    back to original....

--- Assets/AStar.cs
+++ Assets/AStar.cs
@@ -3,21 +3,194 @@ using System.Collections;
 using UnityEngine;
 
 /// <summary>
+/// Base class for pathfinding nodes, it holds no actual information about the map. 
+/// An inherited class must be constructed from this class and all virtual methods must be 
+/// implemented. Note, that calling base() in the overridden methods is not needed.
+/// </summary>
+public class AStarNode : IComparable
+{
+	#region Properties
+	
+	private AStarNode FParent;
+	/// <summary>
+	/// The parent of the node.
+	/// </summary>
+	public AStarNode Parent
+	{
+		get
+		{
+			return FParent;
+		}
+		set
+		{
+			FParent = value;
+		}
+	}
+	
+	/// <summary>
+	/// The accumulative cost of the path until now.
+	/// </summary>
+	public double Cost 
+	{
+		set
+		{
+			FCost = value;
+		}
+		get
+		{
+			return FCost;
+		}
+	}
+	private double FCost;
+	
+	/// <summary>
+	/// The estimated cost to the goal from here.
+	/// </summary>
+	public double GoalEstimate 
+	{
+		set
+		{
+			FGoalEstimate = value;
+		}
+		get 
+		{
+			Calculate();
+			return(FGoalEstimate);
+		}
+	}
+	private double FGoalEstimate;
+	
+	/// <summary>
+	/// The cost plus the estimated cost to the goal from here.
+	/// </summary>
+	public double TotalCost
+	{
+		get 
+		{
+			return(Cost + GoalEstimate);
+		}
+	}
+	
+	/// <summary>
+	/// The goal node.
+	/// </summary>
+	public AStarNode GoalNode 
+	{
+		set 
+		{
+			FGoalNode = value;
+			Calculate();
+		}
+		get
+		{
+			return FGoalNode;
+		}
+	}
+	private AStarNode FGoalNode;
+	
+	#endregion
+	
+	#region Constructors
+	
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	/// <param name="AParent">The node's parent</param>
+	/// <param name="AGoalNode">The goal node</param>
+	/// <param name="ACost">The accumulative cost until now</param>
+	public AStarNode(AStarNode AParent,AStarNode AGoalNode,double ACost)
+	{
+		FParent = AParent;
+		FCost = ACost;
+		GoalNode = AGoalNode;
+	}
+	#endregion
+	
+	#region Public Methods
+	
+	/// <summary>
+	/// Determines wheather the current node is the goal.
+	/// </summary>
+	/// <returns>Returns true if current node is the goal</returns>
+	public bool IsGoal()
+	{
+		return IsSameState(FGoalNode);
+	}
+	
+	#endregion
+	
+	#region Virtual Methods
+	
+	/// <summary>
+	/// Determines wheather the current node is the same state as the on passed.
+	/// </summary>
+	/// <param name="ANode">AStarNode to compare the current node to</param>
+	/// <returns>Returns true if they are the same state</returns>
+	public virtual bool IsSameState(AStarNode ANode)
+	{
+		return false;
+	}
+	
+	/// <summary>
+	/// Calculates the estimated cost for the remaining trip to the goal.
+	/// </summary>
+	public virtual void Calculate()
+	{
+		FGoalEstimate = 0.0f;
+	}
+	
+	/// <summary>
+	/// Gets all successors nodes from the current node and adds them to the successor list
+	/// </summary>
+	/// <param name="ASuccessors">List in which the successors will be added</param>
+	public virtual void GetSuccessors(ArrayList ASuccessors)
+	{
+		
+	}
+	
+	#endregion
+	
+	#region Overridden Methods
+	
+	public override bool Equals(object obj)
+	{
+		return IsSameState((AStarNode)obj);
+	}
+	
+	public override int GetHashCode()
+	{
+		return base.GetHashCode();
+	}
+	
+	#endregion
+	
+	#region IComparable Members
+	
+	public int CompareTo(object obj)
+	{
+		return(-TotalCost.CompareTo(((AStarNode)obj).TotalCost));
+	}
+	
+	#endregion
+}
+
+/// <summary>
 /// Class for performing A* pathfinding
 /// </summary>
 public sealed class AStar
 {
 	#region Private Fields
-
-	private AStarNode2D FStartNode;
-	private ArrayList FOpenList;
-	private ArrayList FClosedList;
+	
+	private AStarNode FStartNode;
+	//private AStarNode FGoalNode;
+	private Heap FOpenList;
+	private Heap FClosedList;
 	private ArrayList FSuccessors;
-
+	
 	#endregion
-
+	
 	#region Properties
-
+	
 	/// <summary>
 	/// Holds the solution after pathfinding is done. <see>FindPath()</see>
 	/// </summary>
@@ -29,139 +202,83 @@ public sealed class AStar
 		}
 	}
 	private ArrayList FSolution;
-
+	
 	#endregion
 	
 	#region Constructors
-
+	
 	public AStar()
 	{
-		FOpenList = new ArrayList();
-		FClosedList = new ArrayList();
+		FOpenList = new Heap();
+		FClosedList = new Heap();
 		FSuccessors = new ArrayList();
 		FSolution = new ArrayList();
 	}
-
+	
 	#endregion
-
-	private AStarNode2D GetCheapest(ArrayList FOpenList)
-	{
-		// Get the node with the lowest TotalCost
-		//AStarNode NodeCurrent = (AStarNode)FOpenList.Pop();
-		AStarNode2D cheapest = (AStarNode2D)FOpenList[0]; // assuming at least one in there
-		for (int x = 0; x < FOpenList.Count; x++) {
-			if (cheapest == null || ((AStarNode2D)FOpenList[x]).TotalCost < cheapest.TotalCost) {
-				cheapest = (AStarNode2D)FOpenList[x];
-			}
-		}
-		return cheapest;
-	}
-
-	private bool HasXY(ArrayList List, AStarNode2D Node) {
-		for (int x = 0; x < List.Count; x++) {
-			if (((AStarNode2D)List[x]).X == Node.X && ((AStarNode2D)List[x]).Y == Node.Y)
-				return true;
-		}
-		return false;
-	}
 	
 	#region Public Methods
-	
+
+	int callcount = 0;
 	/// <summary>
 	/// Finds the shortest path from the start node to the goal node
 	/// </summary>
 	/// <param name="AStartNode">Start node</param>
 	/// <param name="AGoalNode">Goal node</param>
-	public void FindPath(AStarNode2D AStartNode,AStarNode2D AGoalNode)
+	public void FindPath(AStarNode AStartNode,AStarNode AGoalNode)
 	{
 		FStartNode = AStartNode;
-		int count = 0;
-
+		//FGoalNode = AGoalNode;
+		
 		FOpenList.Add(FStartNode);
+		
 		while(FOpenList.Count > 0) 
 		{
-			count++;
-			if (count > 3000) {
-				break;
+			callcount++;
+			if (callcount > 3000) {
+				Debug.Log("openlist is still not empty after 3000 calls");
+				break; // something is wrong
 			}
-			AStarNode2D NodeCurrent = GetCheapest(FOpenList);
+			// Get the node with the lowest TotalCost
+			AStarNode NodeCurrent = (AStarNode)FOpenList.Pop();
 			
 			// If the node is the goal copy the path to the solution array
 			if(NodeCurrent.IsGoal()) {
-				Debug.Log("found goal!");
 				while(NodeCurrent != null) {
-					FSolution.Insert(0, NodeCurrent);
+					FSolution.Insert(0,NodeCurrent);
 					NodeCurrent = NodeCurrent.Parent;
 				}
-				break;
+				break;					
 			}
-
+			
+			// Get successors to the current node
 			NodeCurrent.GetSuccessors(FSuccessors);
-			foreach(AStarNode2D NodeSuccessor in FSuccessors)
+			foreach(AStarNode NodeSuccessor in FSuccessors) 
 			{
-				// Algorithm from policyalmanac.org post-------------
-
-				// If it is not walkable or if it is on the closed list, ignore it.
-				if (HasXY(FClosedList, NodeSuccessor))
-					continue;
-
-				// If it isn’t on the open list, add it to the open list.
-				// Record the F, G, and H costs of the square. 
-				if (!HasXY(FOpenList, NodeSuccessor)) {
-					FOpenList.Add(NodeSuccessor);
-					continue;
-				}
-
-				// Make the current square the parent of this square.
-				// done during get successors
-				//NodeSuccessor.Parent = NodeCurrent;
-
-				// Record the F, G, and H costs of the square.
-				// done during get successors
-
-
-				// If it is on the open list already, check to see if this path to that square is better,
-				// using G cost as the measure. A lower G cost means that this is a better path. If so, 
-				// change the parent of the square to the current square, and recalculate the G and F scores of the square.
-				if (HasXY(FOpenList, NodeCurrent)) {
-					if (NodeCurrent.Cost < NodeSuccessor.Cost) {
-						NodeSuccessor.Parent = NodeCurrent.Parent;
-						NodeSuccessor.Calculate();
-					}
-				}
-
-				// Original algorithm --------------------------------
-
-				// Test if the current successor node is on the open list, if it is and
+				// Test if the currect successor node is on the open list, if it is and
 				// the TotalCost is higher, we will throw away the current successor.
-
-//				AStarNode2D NodeOpen = null;
-//				if(FOpenList.Contains(NodeSuccessor)) {
-//					NodeOpen = (AStarNode2D)FOpenList[FOpenList.IndexOf(NodeSuccessor)];
-//				}
-//				if((NodeOpen != null) && (NodeSuccessor.TotalCost > NodeOpen.TotalCost))
-//					continue;
+				AStarNode NodeOpen = null;
+				if(FOpenList.Contains(NodeSuccessor))
+					NodeOpen = (AStarNode)FOpenList[FOpenList.IndexOf(NodeSuccessor)];
+				if((NodeOpen != null) && (NodeSuccessor.TotalCost > NodeOpen.TotalCost)) 
+					continue;
 				
-				// Test if the current successor node is on the closed list, if it is and
+				// Test if the currect successor node is on the closed list, if it is and
 				// the TotalCost is higher, we will throw away the current successor.
-
-//				AStarNode2D NodeClosed = null;
-//				if(FClosedList.Contains(NodeSuccessor)) {
-//					NodeClosed = (AStarNode2D)FClosedList[FClosedList.IndexOf(NodeSuccessor)];
-//				}
-//				if((NodeClosed != null) && (NodeSuccessor.TotalCost > NodeClosed.TotalCost)) 
-//		 			continue;
+				AStarNode NodeClosed = null;
+				if(FClosedList.Contains(NodeSuccessor))
+					NodeClosed = (AStarNode)FClosedList[FClosedList.IndexOf(NodeSuccessor)];
+				if((NodeClosed != null) && (NodeSuccessor.TotalCost > NodeClosed.TotalCost)) 
+					continue;
 				
 				// Remove the old successor from the open list
-				//FOpenList.Remove(NodeOpen);
-				//FOpenList.Remove(NodeSuccessor);
+				FOpenList.Remove(NodeOpen);
 				
 				// Remove the old successor from the closed list
-				//FClosedList.Remove(NodeClosed);
-				//FClosedList.Remove(NodeSuccessor);
+				FClosedList.Remove(NodeClosed);
 				
 				// Add the current successor to the open list
-				FOpenList.Add(NodeSuccessor);
+				FOpenList.Push(NodeSuccessor);
 			}
 			// Add the current node to the closed list
 			FClosedList.Add(NodeCurrent);
