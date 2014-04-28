e1470f0e4fd4bb40c92c4713e218f43c913b8062
Wolfenhex <wolfenhex@gmail.com>
2014-04-28 12:11:23 -0500

    Got pathfinding working.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -22,43 +22,32 @@ public class Enemy : Moving {
 			player = Map.Player.transform;
 			return;
 		}
-
-		// only calls Chase() once... for testing purposes
-		done = true;
-
-		AStar astar = new AStar();
 		
-		int goalx = Mathf.RoundToInt (player.position.x);
-		int goaly = Mathf.RoundToInt (player.position.y);
-		int startx = Mathf.RoundToInt (transform.position.x);
-		int starty = Mathf.RoundToInt (transform.position.y);
-		AStarNode2D GoalNode = new AStarNode2D(null,null,0,goalx,goaly);
-		AStarNode2D StartNode = new AStarNode2D(null,GoalNode,0,startx,starty);
-		StartNode.GoalNode = GoalNode;
+		int goalX = Mathf.RoundToInt (player.position.x);
+		int goalY = Mathf.RoundToInt (player.position.y);
+		int startX = Mathf.RoundToInt (transform.position.x);
+		int startY = Mathf.RoundToInt (transform.position.y);
 
-		//Debug.Log ("chasing from: " + startx + "," + starty + " to: " + goalx + "," + goaly);
-		
-		astar.FindPath(StartNode,GoalNode);
+		Pathfinder.Point[] points = Pathfinder.FindPath (startX, startY, goalX, goalY);
 
-		if (astar.Solution.Count > 1) {
-			// clear the visualization
-			for (int o = 0; o < vis.Length; o++) {
-				vis[o].renderer.enabled = false;
-			}
-			
-			// let's visualize this
-			AStarNode2D step;
-			for (int s = 0; s < astar.Solution.Count; s++) {
-				step = (AStarNode2D)astar.Solution[s];
-
-				vis[s].renderer.enabled = true;
-				vis[s].transform.position = new Vector2(step.X, step.Y);
-			}
-
-			AStarNode2D firstStep = (AStarNode2D)astar.Solution[1];
-			Move (firstStep.X - startx, firstStep.Y - starty); // just take the first step... you'll recalculate the path next go around
-			//Move (startx, starty); // don't move
+		if(points == null)
+		{
+			return;
 		}
+
+		if(points.Length < 1)
+		{
+			return;
+		}
+
+		Pathfinder.Point point = points[0];
+
+		if(point == null)
+		{
+			return;
+		}
+
+		Move(point.X - startX, point.Y - startY);
 	}
 	
 	void Flee()
@@ -68,8 +57,6 @@ public class Enemy : Moving {
 			return;
 		}
 
-		done = true;
-		
 		if (player.position.y <= transform.position.y) {
 			Move (0, 1);
 		}			
@@ -86,28 +73,14 @@ public class Enemy : Moving {
 	
 	void Start () {
 		renderer.material.color = Color.green;
-		vis = new GameObject[100];
-		for (int x = 0; x < 100; x++) {
-			vis[x] = GameObject.CreatePrimitive(PrimitiveType.Quad);
-			vis[x].renderer.material.color = Color.red;
-			vis[x].transform.localScale = new Vector3(0.5f, 0.5f, 1.0f);
-		}
 	}
-
-	bool done = false;
 
 	void Update () {
 		
-		if (++Frame % 60 > 0) {
+		if (++Frame % 16 > 0) {
 			return;
 		}
 
-		if (done) {
-			//Chase ();
-			// only calls Chase() once
-		}
-		
-		//Flee ();
 		Chase ();
 	}
 }
